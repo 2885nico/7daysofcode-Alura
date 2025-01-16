1b3ad608caa58b70abf3cb9d48306ce4fed31dae
2885nico <104214750+2885nico@users.noreply.github.com>
2025-01-16 11:47:01 -0300

    Create Dia2_Solucion.js
    #7DaysOfCode

---
+++ Dia 2/Dia2_Solucion.js
@@ -0,0 +1,5 @@
+nombre=prompt("¿Cuál es tu nombre?: ")
+edad=prompt("¿Cuántos años tienes?: ")
+lenguaje=prompt("¿Qué lenguaje de programación estás estudiando?: ")
+
+alert(`Hola ${nombre}, tienes ${edad} años y ya estas aprendiendo ${lenguaje}`)
